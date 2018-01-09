d7f55595167a87372cdc251519b8dd2e73485084
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-09 11:28:10 +0100

    add beans

---
+++ src/main/java/com/github/przemyslawkonik/bean/LoginData.java
@@ -0,0 +1,26 @@
+package com.github.przemyslawkonik.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginData {
+	private String email;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
