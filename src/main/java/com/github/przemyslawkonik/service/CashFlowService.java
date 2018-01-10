12b1339f574f75e4cf671021fc9721dc7cba3dfa
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-10 17:33:12 +0100

    cash flow service

---
+++ src/main/java/com/github/przemyslawkonik/service/CashFlowService.java
@@ -0,0 +1,41 @@
+package com.github.przemyslawkonik.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.przemyslawkonik.entity.CashFlow;
+import com.github.przemyslawkonik.entity.User;
+import com.github.przemyslawkonik.repository.CashFlowRepository;
+import com.github.przemyslawkonik.repository.UserRepository;
+import com.github.przemyslawkonik.service.user.UserService;
+
+@Service
+@Transactional
+public class CashFlowService {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private CashFlowRepository cashFlowRepo;
+
+	public void addOrEditCashFlow(CashFlow cashFlow) {
+		User user = userRepo.findOne(userService.getSessionUser().getId());
+		user.updateMoney(cashFlow);
+		userRepo.save(user);
+		cashFlowRepo.save(cashFlow);
+	}
+
+	public void deleteCashFlow(long id) {
+		CashFlow cashFlow = cashFlowRepo.findOne(id);
+		User user = userRepo.findOne(userService.getSessionUser().getId());
+		user.updateMoneyOnDelete(cashFlow);
+		userRepo.save(user);
+		cashFlowRepo.delete(cashFlow);
+	}
+}
