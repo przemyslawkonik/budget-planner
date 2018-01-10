0b1093323a57944261ee1fd237125642f79d0a90
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-10 19:44:29 +0100

    plan and budget entity

---
+++ src/main/java/com/github/przemyslawkonik/entity/Budget.java
@@ -0,0 +1,88 @@
+package com.github.przemyslawkonik.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "budget")
+public class Budget {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private int month;
+
+	private int year;
+
+	@OneToMany
+	private Set<Plan> plans;
+
+	@OneToMany
+	private Set<Plan> cashFlows;
+
+	@ManyToOne
+	private User user;
+
+	public Budget() {
+		plans = new HashSet<>();
+		cashFlows = new HashSet<>();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Set<Plan> getPlans() {
+		return plans;
+	}
+
+	public void setPlans(Set<Plan> plans) {
+		this.plans = plans;
+	}
+
+	public Set<Plan> getCashFlows() {
+		return cashFlows;
+	}
+
+	public void setCashFlows(Set<Plan> cashFlows) {
+		this.cashFlows = cashFlows;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
---
+++ src/main/java/com/github/przemyslawkonik/entity/Plan.java
@@ -0,0 +1,56 @@
+package com.github.przemyslawkonik.entity;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "plan")
+public class Plan {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private BigDecimal value;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+	private Category category;
+
+	public Plan() {
+		value = new BigDecimal("0");
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValue() {
+		return value.setScale(2, RoundingMode.DOWN);
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
