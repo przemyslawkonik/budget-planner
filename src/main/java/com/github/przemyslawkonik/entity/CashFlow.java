dc452f6f95165cd47d16f4db2b5deb3c0f9d6038
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-10 13:48:16 +0100

    cash flow entity, mapping, category entity changes

---
+++ src/main/java/com/github/przemyslawkonik/entity/CashFlow.java
@@ -0,0 +1,51 @@
+package com.github.przemyslawkonik.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cash_flow")
+public class CashFlow {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private BigDecimal value;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+	private Category category;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
--- src/main/java/com/github/przemyslawkonik/entity/Category.java
+++ src/main/java/com/github/przemyslawkonik/entity/Category.java
@@ -1,5 +1,8 @@
 package com.github.przemyslawkonik.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -30,6 +34,13 @@ public class Category {
 
 	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
 	private User user;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "category", cascade = CascadeType.REMOVE)
+	private Set<CashFlow> cashflows;
+
+	public Category() {
+		cashflows = new HashSet<>();
+	}
 
 	public long getId() {
 		return id;
@@ -61,6 +72,14 @@ public class Category {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Set<CashFlow> getCashflows() {
+		return cashflows;
+	}
+
+	public void setCashflows(Set<CashFlow> cashflows) {
+		this.cashflows = cashflows;
 	}
 
 }
