857d85f3daad5e26469bbae5a99e5a57011a5b5d
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-10 21:57:32 +0100

    update repositories

--- src/main/java/com/github/przemyslawkonik/repository/BudgetRepository.java
+++ src/main/java/com/github/przemyslawkonik/repository/BudgetRepository.java
@@ -3,7 +3,9 @@ package com.github.przemyslawkonik.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.przemyslawkonik.entity.Budget;
+import com.github.przemyslawkonik.entity.User;
 
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
 
+	public boolean existsByUserAndYearAndMonth(User user, int year, int month);
 }
--- src/main/java/com/github/przemyslawkonik/repository/PlanRepository.java
+++ src/main/java/com/github/przemyslawkonik/repository/PlanRepository.java
@@ -1,9 +1,13 @@
 package com.github.przemyslawkonik.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.github.przemyslawkonik.entity.Budget;
 import com.github.przemyslawkonik.entity.Plan;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
+	public Set<Plan> findByBudget(Budget budget);
 }
