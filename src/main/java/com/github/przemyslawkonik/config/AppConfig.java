13248ea63e4080acdf5f7dc0645ba786ce4235c3
przemyslaw <przemyslawkonik@gmail.com>
2018-02-07 14:26:29 +0100

    heroku deploy

--- src/main/java/com/github/przemyslawkonik/config/AppConfig.java
+++ src/main/java/com/github/przemyslawkonik/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,12 +28,32 @@ import com.github.przemyslawkonik.converter.PaymentMethodConverter;
 import com.github.przemyslawkonik.converter.PlanConverter;
 import com.github.przemyslawkonik.converter.UserConverter;
 
+import java.util.Properties;
+
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan(basePackages = { "com.github.przemyslawkonik" })
 @EnableJpaRepositories(basePackages = { "com.github.przemyslawkonik.repository" })
 public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/budgetDB");
+		driverManagerDataSource.setUsername("root");
+		driverManagerDataSource.setPassword("coderslab");
+
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		if(dbUrl!=null) {
+			driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+			driverManagerDataSource.setUrl(dbUrl);
+			driverManagerDataSource.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
+			driverManagerDataSource.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+		}
+		return driverManagerDataSource;
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -49,6 +70,17 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	public LocalEntityManagerFactoryBean entityManagerFactory() {
 		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
 		emfb.setPersistenceUnitName("budgetDB");
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		if(dbUrl!=null) {
+			Properties jpaProperties = new Properties();
+			jpaProperties.put("javax.persistence.jdbc.url", System.getenv("JDBC_DATABASE_URL"));
+			jpaProperties.put("javax.persistence.jdbc.user", System.getenv("JDBC_DATABASE_USERNAME"));
+			jpaProperties.put("javax.persistence.jdbc.password", System.getenv("JDBC_DATABASE_PASSWORD"));
+			jpaProperties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+			jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+			emfb.setJpaProperties(jpaProperties);
+			emfb.afterPropertiesSet();
+		}
 		return emfb;
 	}
 
